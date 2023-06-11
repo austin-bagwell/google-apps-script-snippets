4d3b30123fc5eb21da21a0e8266fdaf9e09c538b
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-06-11 08:36:46 -0400

    add basic methods, basic error handling

--- sheets/models/Array2D.js
+++ sheets/models/Array2D.js
@@ -2,14 +2,15 @@
  * {Array[][]} data
  */
 
+// consider renaming class to Datarange to line up with range.getDatarange().getValues() in GAS
 // different implementation if !headers ?
 export default class Array2D {
   constructor(data) {
     this.dataRange = data;
-    this.headers = data.slice(0, 1);
-    this.bodyData = data.slice(1);
-    this.rowLen = data.length;
-    this.columnLen = data[0].length;
+    this.headers = this.dataRange.slice(0, 1);
+    this.bodyData = this.dataRange.slice(1);
+    this.rowLen = this.dataRange.length;
+    this.columnLen = this.dataRange[0].length;
   }
 
   // does this go in constructor?
@@ -17,32 +18,61 @@ export default class Array2D {
 
   // get longest row / set longest row ?
   // idk if getters and setters work in GAS
-  longestRow = { idx: 0, len: 0 };
+  //   longestRow = { idx: 0, len: 0 };
 
-  getCell(x, y) {
-    return this.data[x][y];
+  getCell(row, col) {
+    if (!row || !col) {
+      throw Error("Parameters row & col are required");
+    }
+
+    if (!Number.parseInt(row) || !Number.parseInt(col)) {
+      throw Error("Arguments to row & col must be a number");
+    }
+
+    return this.dataRange[row][col];
+  }
+
+  getRow(row) {
+    if (!Number.parseInt(row)) {
+      throw Error("Argument to row must be a number");
+    }
+
+    if (row > this.rowLen - 1) {
+      throw Error("Row index out of bounds");
+    }
+
+    return this.dataRange[row];
   }
 
   getRows(start, end) {
     return this.data.slice(start, end);
   }
 
-  getColumns(column) {}
+  getColumn(col) {}
 
-  //   is this the formula I want? do I even know? lol
-  getDistance({ x1, y1, x2, y2 }) {
-    // sqrt (x2 - x1)^2
-    const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-    return distance;
-  }
+  getColumns(col) {}
+
+  //   ex you want a new 2D array with only column indexes [0,2,3]
+  // returns new Array2D
+  reduceToXColumns(headerIdx = []) {}
 
   // like slice but 2D
   // different behavior if !headers ?
-  slice2D({ x1, x2, y1, y2 }) {}
+  slice2D({ row1, row2, col1, col2 }) {}
 
   // option to only convert a specific range? or just the whole array?
   // option to select only specific headers?
+  // see existing convert2DArrayToArrayOfObjects for implementation details
   convertToJson(data, range = false) {}
+
+  // is this the formula I want? do I even know? lol
+  // where Array2D[0][0] = { row:0, col:0 }
+  getDistance({ row1, col1, row2, col2 }) {
+    const distance = Math.sqrt(
+      Math.pow(row2 - row1, 2) + Math.pow(col2 - col1, 2)
+    );
+    return distance;
+  }
 
   //   use this to make sure all rows maintain the same length
   // all rows as long as the longest row
--- sheets/tests/testArray2D.js
+++ sheets/tests/testArray2D.js
@@ -1,15 +1,18 @@
 import Array2D from "../models/Array2D.js";
 
+// Array2D[0][0] === "0A"
 const testData = [
-  ["0A", "1B", "2C", "3D", "4E"],
-  ["0A", "1B", "2C", "3D", "4E"],
-  ["0A", "1B", "2C", "3D", "4E"],
-  ["0A", "1B", "2C", "3D", "4E"],
-  ["0A", "1B", "2C", "3D", "4E"],
-  ["0A", "1B", "2C", "3D", "4E"],
+  ["0A", "0B", "0C", "0D", "0E"],
+  ["1A", "1B", "1C", "1D", "1E"],
+  ["2A", "2B", "2C", "2D", "2E"],
+  ["3A", "3B", "3C", "3D", "3E"],
+  ["4A", "4B", "4C", "4D", "4E"],
+  ["5A", "5B", "5C", "5D", "5E"],
 ];
 
 const array2D = new Array2D(testData);
-const distance1 = array2D.getDistance({ x1: 0, y1: 0, x2: 1, y2: 1 });
-console.log(distance1);
-// console.table(array2D.dataRange);
+// console.log(array2D.rowLen);
+
+const row1 = array2D.getRow(6);
+
+console.log(row1);
